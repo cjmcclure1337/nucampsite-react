c2d1fe09191203a9d7469f6ff203c33f3aed1cd8
Chris <cjmcclure1337@gmail.com>
2021-03-12 19:26:26 -0600

    Add Home Page Content

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -39,22 +39,22 @@ class Header extends Component {
                             <Nav navbar>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/home">
-                                        <i classname="fa fa-home fa-lg" /> Home
+                                        <i className="fa fa-home fa-lg" /> Home
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/directory">
-                                        <i classname="fa fa-list fa-lg" /> Directory
+                                        <i className="fa fa-list fa-lg" /> Directory
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/aboutus">
-                                        <i classname="fa fa-info fa-lg" /> About Us
+                                        <i className="fa fa-info fa-lg" /> About Us
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/contactus">
-                                        <i classname="fa fa-address-card fa-lg" /> Contact Us
+                                        <i className="fa fa-address-card fa-lg" /> Contact Us
                                     </NavLink>
                                 </NavItem>
                             </Nav>
