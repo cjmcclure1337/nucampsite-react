13bec6c7351b06b22e2570f8e9b7398f5fa17ea7
Chris <cjmcclure1337@gmail.com>
2021-03-22 19:29:08 -0500

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Button, Card, CardBody, CardText, CardImg, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Label, Col, Row} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors} from "react-redux-form";
+import {Loading} from "./LoadingComponent";
 
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
@@ -42,6 +43,28 @@ function RenderComments({comments, addComment, campsiteId}) {
 }
 
 function CampsiteInfo(props) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Loading />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     if(props.campsite) {
         return (
             <div className="container">
