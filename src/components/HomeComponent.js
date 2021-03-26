5428df81c6c4bfcb4a12250b4194913739be3c2d
Chris <cjmcclure1337@gmail.com>
2021-03-25 19:13:24 -0500

    Directory Animation Adjustment

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 import {Loading} from "./LoadingComponent";
 import {baseUrl} from "../shared/baseUrl";
